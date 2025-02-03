2f0ba0d2c5bcf4574f0548137fc88a56e96f24ee
Andrielly <andry_psa@hotmail.com>
2025-02-02 22:23:39 -0300

    inclusão de desafio

---
+++ 1001/Soluçao.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Soluçao {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        
+        int A = teclado.nextInt();
+        int B = teclado.nextInt();
+        int X = A + B;
+
+        System.out.println("X = " + X);
+
+        teclado.close();
+    }
+}
